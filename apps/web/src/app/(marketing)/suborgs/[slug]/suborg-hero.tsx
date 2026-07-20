e675d67c591c5eae3e496b168ce3ef1c34743d4b
Mystery335 <pear8423chen@gmail.com>
2026-07-19 19:05:16 -0500

    Suborgs Responsive to screen sizes

--- apps/web/src/app/(marketing)/suborgs/[slug]/suborg-hero.tsx
+++ apps/web/src/app/(marketing)/suborgs/[slug]/suborg-hero.tsx
@@ -18,7 +18,7 @@ export default function SuborgHero(suborg: Suborg) {
 	return (
 		<>
 			<div
-				className="mx-auto grid max-w-screen-xl grid-cols-5 border-2 border-acm-darker-blue/50"
+				className="mx-auto grid w-[90%] max-w-screen-xl grid-cols-5 border-2 border-acm-darker-blue/50"
 				style={{
 					borderColor: modifyColor(colors.poppy, 0.5),
 				}}
@@ -29,10 +29,10 @@ export default function SuborgHero(suborg: Suborg) {
 						backgroundColor: colors.poppy,
 					}}
 				>
-					<h1 className="text-center font-chillax text-9xl font-black text-white">
+					<h1 className="text-center font-chillax text-6xl sm:text-6xl md:text-7xl lg:text-9xl font-black text-white">
 						{name.toLowerCase()}
 					</h1>
-					<h2 className="text-center font-calsans text-2xl font-bold leading-tight tracking-wide text-white">
+					<h2 className="text-center font-calsans text-lg md:text-xl lg:text-2xl font-bold leading-tight tracking-wide text-white">
 						{shortDesc}
 					</h2>
 				</div>
@@ -43,7 +43,7 @@ export default function SuborgHero(suborg: Suborg) {
 					}}
 				>
 					<h1
-						className="text-left font-calsans text-5xl font-bold leading-tight tracking-wide"
+						className="text-left font-calsans text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-tight tracking-wide"
 						style={{
 							color: colors.poppy,
 						}}
@@ -81,7 +81,7 @@ export default function SuborgHero(suborg: Suborg) {
 				>
 
 
-					<h1 className="text-center font-chillax text-9xl font-black "
+					<h1 className="text-center font-chillax text-5xl sm:text-5xl md:text-7xl lg:text-9xl font-black "
 						style={{
 							borderColor: modifyColor(colors.poppy, 0.5),
 							color: colors.poppy,
@@ -96,7 +96,7 @@ export default function SuborgHero(suborg: Suborg) {
 					}}>
 
 					<h1
-						className="text-center font-calsans text-5xl font-bold leading-tight tracking-wide"
+						className="text-center font-calsans text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold leading-tight tracking-wide"
 						style={{
 							color: colors.poppy,
 						}}
@@ -112,7 +112,7 @@ export default function SuborgHero(suborg: Suborg) {
 								/>
 							}
 							bgColor={modifyColor(colors.poppy, 0.25)}
-						><span className="font-bold text-3xl ">{name}</span>
+						><span className="font-bold sm:text-lg md:text-xl lg:text-3xl ">{name}</span>
 						</Pill> {aboutUs_One}
 						<Pill
 							icon={
@@ -124,7 +124,7 @@ export default function SuborgHero(suborg: Suborg) {
 								/>
 							}
 							bgColor={modifyColor(colors.poppy, 0.25)}
-						><span className="font-bold text-3xl ">{name}</span>
+						><span className="font-bold sm:text-lg md:text-xl lg:text-3xl ">{name}</span>
 						</Pill> {aboutUs_Two}
 					</h1>
 				</div>
@@ -145,7 +145,7 @@ export default function SuborgHero(suborg: Suborg) {
 					}}>
 
 
-					<h1 className="text-center font-chillax text-9xl font-black "
+					<h1 className="text-center font-chillax text-5xl sm:text-5xl md:text-7xl lg:text-9xl font-black "
 						style={{
 							color: colors.poppy,
 							borderColor: modifyColor(colors.poppy, 0.5),
@@ -154,7 +154,7 @@ export default function SuborgHero(suborg: Suborg) {
 					</h1>
 				</div>
 
-				<div className="col-span-5 grid grid-cols-3 gap-4 border text-center font-chillax text-2xl font-bold "
+				<div className="col-span-5 grid grid-cols-3 gap-4 border text-center font-chillax sm:text-lg md:text-xl lg:text-2xl font-bold "
 					style={{
 						color: colors.poppy,
 						borderColor: modifyColor(colors.poppy, 0.5),
@@ -163,7 +163,7 @@ export default function SuborgHero(suborg: Suborg) {
 						style={{
 							borderColor: modifyColor(colors.poppy, 0.5),
 						}}>
-						<h1 className="text-center font-chillax text-5xl font-bold p-5">
+						<h1 className="text-center font-chillax text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold p-5">
 							{missionHead}
 						</h1>
 						<p>{mPhrase1}</p>
@@ -172,7 +172,7 @@ export default function SuborgHero(suborg: Suborg) {
 						style={{
 							borderColor: modifyColor(colors.poppy, 0.5),
 						}}>
-						<h1 className="text-center font-chillax text-5xl font-bold p-5">
+						<h1 className="text-center font-chillax text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold p-5">
 							{missionHead2}
 						</h1>
 						<p>{mPhrase2}</p>
@@ -182,7 +182,7 @@ export default function SuborgHero(suborg: Suborg) {
 							borderColor: modifyColor(colors.poppy, 0.5),
 						}}>
 
-						<h1 className="text-center font-chillax text-5xl font-bold p-5">
+						<h1 className="text-center font-chillax text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold p-5">
 							{missionHead3}
 						</h1>
 						<p>{mPhrase3}</p>
@@ -193,14 +193,14 @@ export default function SuborgHero(suborg: Suborg) {
 			<div className="p-5"></div>
 
 			<div
-				className="mx-auto bg-[url('/img/landing/noise.png')] grid max-w-screen-xl grid-cols-5 border-2 border-acm-darker-blue/50 p-10"
+				className="mx-auto bg-[url('/img/landing/noise.png')] grid w-[90%] max-w-screen-xl grid-cols-5 border-2 border-acm-darker-blue/50 p-10"
 				style={{
 					borderColor: modifyColor(colors.poppy, 0.5),
 					background: colors.poppy,
 				}}
 			>
 				<div
-					className={`col-span-5 flex-col items-center justify-center text-white text-center font-chillax text-8xl font-black`}
+					className={`col-span-5 flex-col items-center justify-center text-white text-center font-chillax text-4xl sm:text-5xl md:text-5xl lg:text-8xl font-black`}
 				>
 					<h1 >
 						<span className="underline">Join</span> {name}
@@ -209,14 +209,18 @@ export default function SuborgHero(suborg: Suborg) {
 				<div
 					className=" relative col-span-3 flex flex-col items-start justify-center p-5"
 				>
-					<Image className="opacity-90 max-h-[450px] max-w-full object-contain" src={logoUrl.includes("icpc") ? "/img/logos/suborgs/icpc-white.png" : logoUrl} alt={name} width={logoUrl.includes("icpc") ? 400 : 500} height={0} />
-				</div>
-
-				<div
-					className="relative col-span-2 flex flex-col items-start justify-center"
+						<Image
+							className="h-auto w-[140px] object-contain opacity-90 sm:w-[180px] md:w-[220px] lg:w-[280px]"
+							src={logoUrl.includes("icpc") ? "/img/logos/suborgs/icpc-white.png" : logoUrl}
+							alt={name}
+							width={logoUrl.includes("icpc") ? 400 : 500}
+							height={0}
+						/>
+						</div>
+				<div	className="relative col-span-2 flex flex-col items-start justify-center"
 				>
 					<a href="https://portal.acmutsa.org/" target="_blank"
-						className="hover:underline cursor-pointer absolute bottom-0 right-0 font-calsans text-8xl font-bold tracking-wide text-white"
+						className="hover:underline cursor-pointer absolute bottom-0 right-0 whitespace-nowrap font-calsans text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold tracking-wide text-white"
 					>
 						{"Become a Member >"}
 					</a>
@@ -225,7 +229,7 @@ export default function SuborgHero(suborg: Suborg) {
 
 			<div className="p-5"></div>
 
-			<div className="mx-auto grid max-w-screen-xl text-8xl justify-center border-[5px] border-dashed font-bold font-chillax p-5"
+			<div className="mx-auto grid w-[90%] max-w-screen-xl text-3xl sm:text-3xl md:text-6xl lg:text-8xl justify-center border-[5px] border-dashed font-bold font-chillax p-5"
 				style={{
 					color: colors.poppy,
 					borderColor: modifyColor(colors.poppy, 0.5),
@@ -236,7 +240,7 @@ export default function SuborgHero(suborg: Suborg) {
 			<div className="p-5"></div>
 
 			<div
-				className="mx-auto grid max-w-screen-xl grid-cols-5"
+				className="mx-auto grid w-[90%] max-w-screen-xl grid-cols-5"
 			>
 
 				<div className="col-span-5 grid grid-cols-2 gap-4 border text-center font-chillax text-2xl font-bold "
@@ -246,13 +250,14 @@ export default function SuborgHero(suborg: Suborg) {
 
 					}}>
 
-					<div className="relative border-r border-white p-20 text-white text-7xl">
+					<div className="relative border-r border-white p-20 text-white text-2xl sm:text-3xl md:text-5xl lg:text-7xl">
 						<a href={discordLink} target="_blank" className="hover:underline">
 							{"Join Our Discord >"}</a>
-						<Image className="absolute top-12 left-15" src="/img/other/Discord Logo.png" alt="Discord Logo" width={75} height={57} />
+						<Image className="absolute top-10 left-12 sm:top-9 sm:left-9 md:top-9 md:left-12 lg:top-12 lg:left-15  
+						h-8 w-10 sm:h-10 sm:w-12 md:h-12 md:w-16 lg:h-14 lg:w-18" src="/img/other/Discord Logo.png" alt="Discord Logo" width={75} height={57} />
 					</div>
 
-					<div className=" p-20 text-7xl text-white">
+					<div className=" p-20 text-2xl sm:text-3xl md:text-5xl lg:text-7xl text-white">
 						<a href = "/events" target = "_blank" className="hover:underline cursor-pointer">
 							{"Check Out Our Events >"}
 						</a>
@@ -277,19 +282,24 @@ function Pill({
 	icon,
 	children,
 	bgColor,
+	className,
 }: {
 	icon: React.ReactNode;
 	children: React.ReactNode;
 	bgColor?: string;
+	className?: string;
 }) {
 	return (
 		<span
-			className="inline-flex h-10 items-center rounded-full px-4"
+			className={`inline-flex h-6 md:h-8 lg:h-10 items-center rounded-full px-2 lg:px-3`}
 			style={{
 				backgroundColor: bgColor,
 			}}
 		>
-			{cloneElement(icon as React.ReactElement<any>, { size: 20 })}
+			{cloneElement(icon as React.ReactElement<any>, {
+				size: 20,
+				className: "h-4 w-4 sm:h-5 sm:w-5 md:h-6 md:w-6 lg:h-7 lg:w-7",
+			})}
 			<span className="ml-2">{children}</span>
 		</span>
 	);
